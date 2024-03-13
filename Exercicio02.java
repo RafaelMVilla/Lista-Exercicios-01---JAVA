f33c3e2a7104ec1eabce737b96fda295a9a0c126
RafaelMVilla <rafael.march.v@gmail.com>
2024-03-13 11:21:53 -0300

    Exercicio2

---
+++ Exercicio02.java
@@ -0,0 +1,22 @@
+
+/*
+ Escreva um algoritmo que leia dois números digitados pelo usuário e exiba o resultado da sua soma.
+ */
+
+public class Exercicio02 {
+    
+
+    public static void main(String[] args) {
+        
+        double valor1 = Prompt.lerDecimal("Digite o primeiro número: ");
+
+        double valor2 = Prompt.lerDecimal("Digite o segundo número: ");
+
+        double soma = valor1 + valor2;
+
+        Prompt.imprimir("O resultado da soma é: " + soma);
+
+        
+        
+    }
+}
