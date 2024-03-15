290044ab8f04407b65735b1410ab46f8bf861049
RafaelMVilla <rafael.march.v@gmail.com>
2024-03-15 11:34:31 -0300

    Exercicio15

---
+++ Exercicio15.java
@@ -0,0 +1,19 @@
+/*
+Tendo como dados de entrada dois pontos quaisquer no plano, P1(x1, y1) e P2(x2, y2), calcule e retorne a distância entre eles. A fórmula que efetua tal cálculo é: d = raiz (((x2 - x1)2) + ((y2 - y1)2)). Exemplo: p1(0, 5), p2(10, 20). Distancia: 18,03
+ */
+
+public class Exercicio15 {
+    
+    public static void executar(){
+
+        double x1 = Prompt.lerInteiro("Informe as coordenadas cartesianas do primeiro ponto: \n X1 =");
+        double x2 = Prompt.lerInteiro("Informe as coordenadas cartesianas do primeiro ponto: \n Y1 =");
+        double y1 = Prompt.lerInteiro("Informe as coordenadas cartesianas do segundo ponto: \n X2 =");
+        double y2 = Prompt.lerInteiro("Informe as coordenadas cartesianas do segundo ponto: \n Y2 =");
+
+        double d = Math.sqrt ((Math.pow(x2 - x1, 2)) + (Math.pow(y2 - y1, 2)));
+
+        Prompt.imprimir("d = " + d);
+
+    }
+}
